e49780bdf5c28f2980201caa8df0b64c236c6509
suren19 <30802299+suren19@users.noreply.github.com>
2026-05-26 10:18:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const ASSETS = [
   './style.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Install: cache app shell
